e46f1de7e06d7aedc5fc37c366723dfe9005e176
huyennguyenthanh <ngthanhhuyen1707@gmail.com>
2022-08-19 13:38:17 +0700

    Chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,18 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: %i(show new create)
+  before_action :load_user, except: %i(index new create)
+  before_action :admin_user, only: :destroy
+  before_action :correct_user, only: %i(show edit update)
+
   def new
     @user = User.new
   end
 
-  def show
-    @user = User.find_by id: params[:id]
-    return if @user
-
-    flash[:error] = t ".not_found"
-    redirect_to root_path
+  def index
+    @pagy, @users = pagy(User.all, page: params[:page], items: Settings.pagination.item_per_page)
   end
+
+  def show; end
 
   def create
     @user = User.new user_params
@@ -23,9 +26,57 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t ".update_success"
+      redirect_to @user
+    else
+      flash.now[:danger] = t ".update_fail"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t ".destroy_success"
+    else
+      flash[:danger] = t ".destroy_fail"
+    end
+    redirect_to users_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "users.login_require_message"
+    redirect_to login_path
+  end
+
+  def correct_user
+    return if current_user?(@user)
+
+    flash[:error] = t ".update_correct_user_mess"
+    redirect_to root_url
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:danger] = t ".not_found_user"
+    redirect_to root_path
+  end
+
+  def admin_user
+    redirect_to root_path unless current_user.admin?
   end
 end
